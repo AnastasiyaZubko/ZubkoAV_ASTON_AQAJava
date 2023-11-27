817d902f23f2c685c63c0ef85ff555b6262108cb
Anastasiya <nastya.suhodolova@mail.ru>
2023-11-27 21:04:18 +0300

    Lesson_4_PARK_and_Carousel

--- src/Lesson_4/Park.java
+++ src/Lesson_4/Park.java
@@ -1,5 +1,8 @@
 package Lesson_4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Park {
 
     public static class Carousel {
@@ -23,4 +26,26 @@ public class Park {
         }
     }
 
+}
+ class CarouselList {
+    private static final List<Park.Carousel> carouselList = new ArrayList<>();
+
+    static {
+        carouselList.add(0, new Park.Carousel("Веселые Горки", 5, "10.00", "20.00"));
+        carouselList.add(1, new Park.Carousel("Колесо обозрения", 10, "10.00", "20.00"));
+        carouselList.add(2, new Park.Carousel("Батут", 2, "10.00", "18.00"));
+    }
+
+    public static List<Park.Carousel> getCarouselList() {
+        return carouselList;
+    }
+}
+
+class InfoCarousel {
+    public static void main(String[] args) {
+        System.out.println("Парк Атракционов Веселушки ");
+        for (int i = 0; i < CarouselList.getCarouselList().size(); i++) {
+            System.out.println(CarouselList.getCarouselList().get(i));
+        }
+    }
 }
